24419b75bc471224bc5129bdd4528f3d2b70aec0
Denis Kusmitschev <deniskusmitschev@outlook.de>
2023-11-28 14:39:41 +0100

    added backend and signup.js

---
+++ js/sign_up.js
@@ -0,0 +1,41 @@
+async function initRegister() {
+    loadUsers();
+}
+
+async function loadUsers() {
+    try {
+        users = JSON.parse(await getItem('users'));
+    } catch (e) {
+        console.error('Loading error:', e);
+    }
+}
+
+
+async function register() {
+
+    let newUser = document.getElementById('newUser');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    users.push({ newUser: newUser.value, email: email.value, password: password.value, });
+    setTimeout(moveToLogin, 3000);
+    moveToLogin();
+    await setItem('users', JSON.stringify(users));
+    resetForm();
+}
+
+
+function moveToLogin() {
+    window.location.href = 'log-in.html?msg=Hat alles geklappt';
+    const urlParams = new URLSearchParams(window.location.search);
+    const msg = urlParams.get('msg');
+    if (msg) {
+        msgBox.innerHTML = msg;
+    }
+}
+
+function resetForm() {
+    newUser.value = '';
+    email.value = '';
+    password.value = '';
+    confirmPassword.value = '';
+}
