9bd3c269b8b43ab12926a9cdd5b863e4216dca2a
Denis Kusmitschev <deniskusmitschev@outlook.de>
2023-12-07 14:24:45 +0100

    updated log in 1.1

--- js/login.js
+++ js/login.js
@@ -1,9 +1,17 @@
 function login() {
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-    let user = users.find(u => u.email === email && u.password === password);
-    console.log(user);
-}
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = users.find(u => u.email === email.value && u.password === password.value);
+  
+    if (!user) {
+      email.classList.add('wrong');
+      password.classList.add('wrong');
+    } else {
+      window.location.href = 'summary.html';
+    }
+    email.innerHTML = '';
+    password.innerHTML = '';
+  }
 
 function guestLogIn() {
     window.location.href = 'summary.html'
