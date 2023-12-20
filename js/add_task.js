90473173ec59bcc1ad79874bbe0a7db7021c2f53
Denis Kusmitschev <deniskusmitschev@outlook.de>
2023-12-20 14:19:29 +0100

    addTask update

--- js/add_task.js
+++ js/add_task.js
@@ -3,6 +3,7 @@ let status = ''; // status added
 let tasks = [];
 let assignedTo = [];
 let urgentPrio = [];
+let contactsDisplayed = false;
 
 
 async function initTasks() {
@@ -20,26 +21,40 @@ async function loadTask() {
 
 
 async function addTask() {
+    await pushToTask();
+    console.log(tasks);
+}
+
+
+async function pushToTask() {
     let status = "ToDo";  // status added
     let title = document.getElementById('titleInput').value;
     let description = document.getElementById('descriptionInput').value;
     let date = document.getElementById('date').value;
     let category = document.getElementById('category').value;
     let subtask = document.getElementById('subtask').value;
+    let contactDropdown = document.getElementById('assignContact');
+    let selectedContactIndex = contactDropdown.selectedIndex;
+    let selectedContactName = contactDropdown.options[selectedContactIndex].text;
 
-    tasks.push({ title: title, description: description, contact: getContact(), prio: prios, date: date, category: category, subtask: subtask, urgentprio: urgentPrio });
-    await setItem('task', JSON.stringify(tasks));
-    console.log(tasks);
+    if (title === "" || date === "" || category === "") {
+        document.getElementById('titleInput').classList.add('wrong-border');
+        document.getElementById('date').classList.add('wrong-border');
+        document.getElementById('category').classList.add('wrong-border');
+    } else {
+        tasks.push({ title: title, description: description, contact: selectedContactName, prio: prios, date: date, category: category, subtask: subtask, urgentprio: urgentPrio });
+        await setItem('task', JSON.stringify(tasks));
+    }
 }
 
 
 function getPrio(button) {
-    chanceColor(button);
+    prioColor(button);
     prios = button;
 }
 
 
-function chanceColor(button) {  // change color? 
+function prioColor(button) {
     let red = document.getElementById('urgentBtn');
     let yellow = document.getElementById('mediumBtn');
     let green = document.getElementById('lowBtn');
@@ -63,19 +78,54 @@ function chanceColor(button) {  // change color?
 }
 
 
- function getContact() {
-    let dropdown = document.getElementById('assignContact');
-    let test = document.getElementById('test');
-    test.innerHTML = "";
-
-    for (let i = 0; i < contacts.length; i++) {
-      const contact = contacts[i];
-      let option = document.createElement("option");
-      option.text = contact.name;
-      dropdown.add(option);
-      test.innerHTML += `${contact.name}`;
+function getContact() {
+    if (!contactsDisplayed) {
+        let dropdown = document.getElementById('assignContact');
+        for (let i = 0; i < contacts.length; i++) {
+            const contact = contacts[i];
+            let option = document.createElement("option");
+            option.text = contact.name;
+            dropdown.add(option);
+        }
+        contactsDisplayed = true;
     }
+}
 
+
+function addSubtask() {
+    var subtaskInput = document.getElementById('subtask');
+    var subtaskValue = subtaskInput.value;
+    if (subtaskValue) {
+      var subtaskList = document.createElement('ul');
+      var newSubtask = document.createElement('li');
+      newSubtask.appendChild(document.createTextNode(subtaskValue));
+      subtaskList.appendChild(newSubtask);
+      subtaskList.classList.add('list');
+      var subtaskContainer = document.getElementsByClassName('subtask')[0];
+      subtaskContainer.appendChild(subtaskList);
+      subtaskInput.value = '';
+  
+      newSubtask.addEventListener('dblclick', function () {
+        var deleteIcon = document.createElement('img');
+        deleteIcon.src = './img/log-in/delete.png';
+        deleteIcon.onclick = function () {
+          this.parentNode.remove();
+        };
+  
+        var inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.value = this.firstChild.nodeValue;
+  
+        inputField.onblur = function () {
+          newSubtask.firstChild.nodeValue = this.value;
+          this.parentNode.removeChild(this);
+        };
+  
+        this.appendChild(deleteIcon);
+        this.appendChild(inputField);
+        inputField.focus();
+      });
+    }
   }
 
 
